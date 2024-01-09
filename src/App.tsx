e475570c0d280a8d7528486fda6e6f18879831ec
vinayaknayar <nayarvinayak03@gmail.com>
2024-01-09 12:33:18 +0530

    refactor: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SideMenu from "./components/main/menu/sidemenu"
+import Payment from "./components/main/payments/payment"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="flex flex-row">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <SideMenu />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="bg-[#fafafa] min-w-[300px] md:w-screen min-h-screen">
+        <Payment />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
