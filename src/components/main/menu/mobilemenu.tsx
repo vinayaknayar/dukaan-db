d142901212aa06834a79af210beeba245de83d14
vinayaknayar <nayarvinayak03@gmail.com>
2024-01-09 20:50:28 +0530

    fix: wallet credit div's width for mobile screen

--- src/components/main/menu/mobilemenu.tsx
+++ src/components/main/menu/mobilemenu.tsx
@@ -66,7 +66,7 @@ export function MobileMenu() {
 					</SheetDescription>
 				</SheetHeader>
 				<SheetFooter>
-					<div className="bg-[#353C53] w-[180px] m-3 flex flex-row justify-center items-center gap-4 w-full px-1 py-2 rounded">
+					<div className="bg-[#353C53] w-[150px] m-3 flex flex-row justify-center items-center gap-4 px-1 py-2 rounded">
 						<Button
 							variant={"ghost"}
 							className="bg-black/10 hover:bg-black/30"
